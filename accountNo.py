477c9447b1bbad6e87346bd6cfcd7103595fbb9a
Shaikh Faizan Sadique <110897515+FaizanDevOps@users.noreply.github.com>
2024-07-28 02:42:19 +0530

    Update accountNo.py

--- accountNo.py
+++ accountNo.py
@@ -3,9 +3,9 @@ import json
 def lambda_handler(event, context):
     # TODO implement
     print(event)
-    print(len(event['accountNo'])
-    x=len(event)['accountNo'])
-    if x == 8;
+    print(len(event['accountNo']))
+    x=len(event['accountNo'])
+    if x == 8:
         return {
         "statusCode": 200    
         }
